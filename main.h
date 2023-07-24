f4b89b1d7589a02ef57f5338fab3eda1f60e056b
ondussoh <jacobookinyi@gmail.com>
2023-07-24 12:42:01 -0700

    updated main.h

--- main.h
+++ main.h
@@ -1,7 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+
+// Define a struct to hold information about each print format specifier.
+typedef struct print_format
+{
+    char symbol;          // The character that represents the format specifier.
+    int (*print)(va_list arg); // Function pointer to the corresponding print function.
+} prt;
+
+// Function prototypes for the different print functions.
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_mod(va_list arg);
+
+// Function prototype for the custom printf function.
 int _printf(const char *format, ...);
 
 #endif
