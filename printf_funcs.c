e6bd9678d9e7f4b3d00dd6ad0edaaa6a59479582
ondussoh <jacobookinyi@gmail.com>
2023-07-24 13:42:11 -0700

    printf functions

---
+++ printf_funcs.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*function to print a single character*/
+int print_char(va_list arg)
+{
+        putchar(va_arg(arg, int));
+        return (1);
+}
+/*function to print a string*/
+int print_str(va_list arg)
+{
+        int i;
+        char *str = va_arg(arg, char *);
+
+        if (!str)
+            str = "(null)";
+
+        for (i = 0; str[i]; i++)
+            putchar(str[i]);
+
+        return (i);
+}
+/*function to print a percentage sign*/
+int print_mod(va_list arg)
+{
+	(void)arg;
+
+	putchar('%');
+	return (1);
+}
