2440807a79112ac8908d4d9d77c48513f0d9cb99
warrenshiv <warrenshiv@gmail.com>
2023-07-25 04:11:49 -0700

    Modified the files

--- main.h
+++ main.h
@@ -2,22 +2,36 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdio.h>
-#include <limits.h>
+#include <stddef.h>
+#include <stdlib.h>
 
-/*Define a struct to hold information about each print format specifier*/
-typedef struct print_format
+/**
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
+ */
+
+typedef struct structprint
 {
-  char symbol;          /*the cha that represents the format specifier*/
-  int (*print)(va_list arg); /*function pointer to the corresponding print function*/
-} prt;
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
 
-/*Function prototypes for the different print functions*/
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_mod(va_list arg);
-
-/* Function prototype for the custom printf function*/
-int _printf(const char *format, ...);
-
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 #endif
