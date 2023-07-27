f9760a60b67ad3c131c104f6f63648621df07611
warrenshiv <warrenshiv@gmail.com>
2023-07-27 13:17:31 -0700

    Modified handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -3,6 +3,9 @@
  * handle_print - Prints an argument based on its type
  * @fmt: string in which to print the arguments.
  * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
